8f419a2d18284a8b94ebf80cade8bbafc5b0bae5
BTabaku <baftit40@gmail.com>
2024-11-14 14:31:13 +0100

    add route name for index view

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
 Route::get('/index', function () {
     return view('paintsplatter.index');
-});
+})->name('index');
